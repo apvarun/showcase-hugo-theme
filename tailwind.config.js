4c9b30fcfcb61986fde68d0cfd1359f6b7d0f4c0
Varun A P <varunlakshmananap@gmail.com>
2021-07-13 09:21:17 +0200

    Add darkmode setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['layouts/**/*.html', 'themes/showcase/layouts/**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
